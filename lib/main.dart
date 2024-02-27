316a0e130160bbd68fedcf9c8463b2d5bf490835
Juangui <jgmejiaco@gmail.com>
2024-02-27 12:40:22 -0500

    adicion counterScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/presentation/screens/counter_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,8 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(child: Text('Hola mundo'))),
+      home: CounterScreen()
     );
   }
 }
