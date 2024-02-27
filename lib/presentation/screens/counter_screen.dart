12d6ebec53dc0f6de8ef945e670bf416aa9f0c08
Juangui <jgmejiaco@gmail.com>
2024-02-27 12:41:56 -0500

    adicion counterScreen

--- lib/presentation/screens/counter_screen.dart
+++ lib/presentation/screens/counter_screen.dart
@@ -6,7 +6,7 @@ class CounterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-        body: Center(child: Text('Pantalla del contador')),
+        body: Center(child: Text('Pantalla del contador ')),
     );
   }
 }
